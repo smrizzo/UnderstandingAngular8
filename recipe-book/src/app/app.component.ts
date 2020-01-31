aa84061191deb3f88c0dc9db17172507ab97bc4e
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2020-01-30 21:50:40 -0800

    added some components

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  allowNewServer  = false;
 }
 
 // title binded so html is showing this
