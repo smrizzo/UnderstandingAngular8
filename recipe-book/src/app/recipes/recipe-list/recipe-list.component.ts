d18418fa85940862a3cd0fc3330602f5a19bc2b8
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2020-02-06 20:51:40 -0800

    working on databinding part of course

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Coconut Cake', 'Made with coconut', 'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/04/coconut-cake-5.jpg'),
+    new Recipe('Coconut Cake', 'Made with coconut', 'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/04/coconut-cake-5.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
