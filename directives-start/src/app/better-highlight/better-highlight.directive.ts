874e59a1f705af0d9c581486f5c882ab332a63ce
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2020-02-16 11:10:21 -0800

    finished directive section

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,27 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
+
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'blue';
+  @HostBinding('style.backgroundColor') backGroundColor: string;
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
   // this is better because setStyle uses service workers where as the other highlight will throw error if the dom is not ready
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backGroundColor = this.defaultColor;
+  }
+  // listens for when a mouse enters this element
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backGroundColor = this.highlightColor;
+  }
+  // listens for when a mouse leaves this element
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backGroundColor = this.defaultColor;
   }
 }
